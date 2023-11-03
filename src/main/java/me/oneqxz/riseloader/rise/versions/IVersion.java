3704828cc908a11596dd270a620fa393655cf5d9
oneqxz <oneqxz@gmail.com>
2023-11-03 19:23:21 +0800

    fixed last updated time

--- src/main/java/me/oneqxz/riseloader/rise/versions/IVersion.java
+++ src/main/java/me/oneqxz/riseloader/rise/versions/IVersion.java
@@ -5,51 +5,52 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public interface IVersion {
 
     String getVersion();
     long lastUpdated();
 
-    default LocalDateTime convertUtcToLocalTime(LocalDateTime utcTime, ZoneId zoneId) {
-        return utcTime.atZone(ZoneId.of("UTC")).withZoneSameInstant(zoneId).toLocalDateTime();
-    }
-
-    default String calculateTimeDelta(LocalDateTime localTime) {
-        LocalDateTime now = LocalDateTime.now();
-        ZonedDateTime nowInUtc = now.atZone(ZoneId.of("UTC"));
-        ZonedDateTime localTimeInUtc = localTime.atZone(ZoneId.of("UTC"));
-
-        long deltaInSeconds = nowInUtc.toEpochSecond() - localTimeInUtc.toEpochSecond();
-
-        String timeDelta = "";
-        if (deltaInSeconds < 60) {
-            timeDelta = deltaInSeconds + " sec";
-        } else if (deltaInSeconds < 3600) {
-            timeDelta = deltaInSeconds / 60 + " min";
-        } else if (deltaInSeconds < 86400) {
-            timeDelta = deltaInSeconds / 3600 + " hours";
-        } else if (deltaInSeconds < 604800) {
-            timeDelta = deltaInSeconds / 86400 + " days";
-        } else if (deltaInSeconds < 2592000) {
-            timeDelta = deltaInSeconds / 604800 + " weeks ";
-        } else if (deltaInSeconds < 31536000) {
-            timeDelta = deltaInSeconds / 2592000 + " months ";
-        } else {
-            timeDelta = deltaInSeconds / 31536000 + " years";
-        }
-
-        return timeDelta;
-    }
-
-    public static void main(String[] args) {
-    }
-
     default String getUpdateAgo()
     {
-        LocalDateTime utcTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(lastUpdated()), ZoneId.systemDefault());
-        LocalDateTime localTime = convertUtcToLocalTime(utcTime, ZoneId.systemDefault());
-        return calculateTimeDelta(localTime);
+        Instant currentTime = Instant.now();
+        Instant pastTime = Instant.ofEpochMilli(lastUpdated());
+
+        long secondsAgo = ChronoUnit.SECONDS.between(pastTime, currentTime);
+        if (secondsAgo < 60) {
+            return secondsAgo + (secondsAgo == 1 ? " sec" : " secs");
+        }
+
+        long minutesAgo = ChronoUnit.MINUTES.between(pastTime, currentTime);
+        if (minutesAgo < 60) {
+            return minutesAgo + (minutesAgo == 1 ? " min" : " mins");
+        }
+
+        long hoursAgo = ChronoUnit.HOURS.between(pastTime, currentTime);
+        if (hoursAgo < 24) {
+            return hoursAgo + (hoursAgo == 1 ? " hour" : " hours");
+        }
+
+        long daysAgo = ChronoUnit.DAYS.between(pastTime, currentTime);
+        if (daysAgo < 7) {
+            return daysAgo + (daysAgo == 1 ? " day" : " days");
+        }
+
+        long weeksAgo = daysAgo / 7;
+        if (weeksAgo < 4) {
+            return weeksAgo + (weeksAgo == 1 ? " week" : " weeks");
+        }
+
+        LocalDateTime currentDateTime = LocalDateTime.ofInstant(currentTime, ZoneId.of("GMT"));
+        LocalDateTime pastDateTime = LocalDateTime.ofInstant(pastTime, ZoneId.of("GMT"));
+        long monthsAgo = ChronoUnit.MONTHS.between(pastDateTime, currentDateTime);
+        if (monthsAgo < 12) {
+            return monthsAgo + (monthsAgo == 1 ? " month" : " months");
+        }
+
+        long yearsAgo = monthsAgo / 12;
+        return yearsAgo + (yearsAgo == 1 ? " year" : " years");
     }
 
 }
